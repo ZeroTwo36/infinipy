2a855fce259bd5a72a39e78bfbf786bb91128bee
ZeroTwo <zerotwo36@protonmail.com>
2022-01-15 18:19:28 +0100

    Delete errors.py

--- errors.py
+++
@@ -1,9 +0,0 @@
-from requests import HTTPError
-
-
-class InfinipyBaseException(Exception):
-    """Something has failed"""
-
-
-class RequestFailed(HTTPError):
-    """Request failed (Status Code>=400)"""
