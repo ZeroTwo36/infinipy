4df53ebf7040475694d8e70ea08f04a623017b47
ZeroTwo <zerotwo36@protonmail.com>
2022-01-15 17:35:01 +0100

    Leap to V0.2

--- core.py
+++ core.py
@@ -91,23 +91,5 @@ def has_voted_sync(user,bot_for):
     json = resp.json()
     return not not json['hasVoted']
 
-def endpoint_for(user_id):
-    """Determines whether an ID belongs to the /user or to /bots endpoint
-    """
-    error = fetch_bot_sync(user_id)
-    if isinstance(error,RequestFailed):
-            return f'/user/{user_id}'
-    return f'/bots/{user_id}'
+
     
-
-
-class APISession:
-    def __init__(self,id):
-        self.id = id
-        self.endpoint = endpoint_for(self.id)
-
-    def fetch(self):
-        if self.endpoint.startswith("/bots"):
-            return fetch_bot_sync(self.id)
-        return fetch_user_sync(self.id)
-
