b8e9aeedfcb9c8dc4e2ef7c2662b978320785253
ZeroTwo <zerotwo36@protonmail.com>
2022-01-18 16:39:16 +0100

    Create constants.py

---
+++ infinipy/constants.py
@@ -0,0 +1,18 @@
+"""
+InfinityBotList API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic Sync/Async wrapper for the IBL API.
+
+:copyright: (c) 2022-present ZeroTwo36
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'infinipy'
+__author__ = 'ZeroTwo36'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present ZeroTwo36'
+__version__ = '0.3'
+__version_info__ = 'pre-stable-v0.3'
+
