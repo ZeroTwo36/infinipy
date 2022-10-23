666cfded020a00a6b1890c7c90c7fec3a355f0a3
ZeroTwo <zerotwo36@protonmail.com>
2022-10-23 16:03:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
- 
+
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
 ]
- 
+
 setup(
-  name='infinipy',
-  version='0.0.2',
-  description='A very basic API Wrapper around IBL\'s API',
-  long_description=open('README.md').read(),
-  long_description_content_type="text/markdown",
-  url='',  
-  author='ZeroTwo36',
-  author_email='zerotwo36@protonmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords=['ibl','infinipy','infinity','dbl','discord','discord-botlist'], 
-  packages=find_packages(),
-  install_requires=['requests','aiohttp','flask'] 
+    name='infinipy',
+    version='0.0.2',
+    description='A very basic API Wrapper around IBL\'s API',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='',
+    author='ZeroTwo36',
+    author_email='zerotwo36@protonmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['ibl', 'infinipy', 'infinity', 'dbl', 'discord', 'discord-botlist'],
+    packages=find_packages(),
+    install_requires=['requests', 'aiohttp', 'flask']
 )
